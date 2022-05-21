8befc4b8d0e45958505ec60917d4c3d76d62c1fd
HackTheDev <40896559+hackthedev@users.noreply.github.com>
2022-05-22 01:17:46 +0200

    Initial commit

---
+++ Botnet/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace Botnet
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textbox_log = new System.Windows.Forms.TextBox();
+            this.textbox_search = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_ips = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label_404error = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_status = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label_scanned = new System.Windows.Forms.Label();
+            this.textBox_ips = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textbox_log
+            // 
+            this.textbox_log.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textbox_log.Location = new System.Drawing.Point(15, 151);
+            this.textbox_log.MaxLength = 99999;
+            this.textbox_log.Multiline = true;
+            this.textbox_log.Name = "textbox_log";
+            this.textbox_log.ReadOnly = true;
+            this.textbox_log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textbox_log.Size = new System.Drawing.Size(1975, 744);
+            this.textbox_log.TabIndex = 0;
+            // 
+            // textbox_search
+            // 
+            this.textbox_search.Location = new System.Drawing.Point(15, 56);
+            this.textbox_search.Name = "textbox_search";
+            this.textbox_search.Size = new System.Drawing.Size(269, 27);
+            this.textbox_search.TabIndex = 1;
+            this.textbox_search.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textbox_search_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Search term";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(300, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 54);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_ips
+            // 
+            this.label_ips.AutoSize = true;
+            this.label_ips.Location = new System.Drawing.Point(2014, 118);
+            this.label_ips.Name = "label_ips";
+            this.label_ips.Size = new System.Drawing.Size(27, 20);
+            this.label_ips.TabIndex = 6;
+            this.label_ips.Text = "IPs";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "404 Errors: ";
+            // 
+            // label_404error
+            // 
+            this.label_404error.AutoSize = true;
+            this.label_404error.Location = new System.Drawing.Point(119, 42);
+            this.label_404error.Name = "label_404error";
+            this.label_404error.Size = new System.Drawing.Size(17, 20);
+            this.label_404error.TabIndex = 8;
+            this.label_404error.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label_status);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label_scanned);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label_404error);
+            this.groupBox1.Location = new System.Drawing.Point(559, 11);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(545, 125);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Analytics";
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Location = new System.Drawing.Point(230, 42);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(60, 20);
+            this.label_status.TabIndex = 11;
+            this.label_status.Text = "Waiting";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Status:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Sites scanned:";
+            // 
+            // label_scanned
+            // 
+            this.label_scanned.AutoSize = true;
+            this.label_scanned.Location = new System.Drawing.Point(119, 76);
+            this.label_scanned.Name = "label_scanned";
+            this.label_scanned.Size = new System.Drawing.Size(17, 20);
+            this.label_scanned.TabIndex = 10;
+            this.label_scanned.Text = "0";
+            // 
+            // textBox_ips
+            // 
+            this.textBox_ips.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_ips.Location = new System.Drawing.Point(2014, 151);
+            this.textBox_ips.Multiline = true;
+            this.textBox_ips.Name = "textBox_ips";
+            this.textBox_ips.ReadOnly = true;
+            this.textBox_ips.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_ips.Size = new System.Drawing.Size(334, 744);
+            this.textBox_ips.TabIndex = 5;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(2371, 946);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.textbox_log);
+            this.tabPage1.Controls.Add(this.label_ips);
+            this.tabPage1.Controls.Add(this.textbox_search);
+            this.tabPage1.Controls.Add(this.textBox_ips);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(2363, 913);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(242, 92);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2386, 970);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textbox_log;
+        private System.Windows.Forms.TextBox textbox_search;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_ips;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label_404error;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label_scanned;
+        private System.Windows.Forms.TextBox textBox_ips;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ Botnet/Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Botnet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region
+
+        int NotFoundError = 0;
+        int SitesScanned= 0;
+        int IPsAdded = 0;
+
+        bool searching = false;
+
+        string[] blacklistedDomains = new string[] { "google", "youtube", "microsoft", "facebook", "gstatic", "w3" };
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = textbox_search;
+            textbox_search.Focus();
+        }
+
+        public void write(string text)
+        {
+            if (textbox_log.InvokeRequired)
+            {
+                textbox_log.Invoke(new Action<string>(write), new object[] { text });
+                return;
+            }
+            try 
+            { 
+                if (textbox_log.Text.Length > 9000) { textbox_log.Clear(); }
+                textbox_log.AppendText(text + Environment.NewLine); 
+            } 
+            catch
+            { 
+
+            }
+        }
+
+        public void setStatus(string text)
+        {
+            if (label_status.InvokeRequired)
+            {
+                label_status.Invoke(new Action<string>(setStatus), new object[] { text });
+                return;
+            }
+            label_status.Text = text;
+        }
+
+        public void addIp(string text)
+        {
+            bool found = false;
+            if (textBox_ips.InvokeRequired)
+            {
+                textBox_ips.Invoke(new Action<string>(addIp), new object[] { text });
+                return;
+            }
+
+            foreach (string line in textBox_ips.Lines)
+            {
+                if(line == text) { found = true; }
+            }
+
+            if (found == false) // && checkSSH(text) == true 
+            {
+                IPsAdded++;
+                textBox_ips.AppendText(text + Environment.NewLine);
+
+                // ip label counter
+                if (label_ips.InvokeRequired)
+                {
+                    label_ips.Invoke(new Action<string>(addIp), new object[] { text });
+                    return;
+                }
+                label_ips.Text = "IPs (" + IPsAdded.ToString() + ")";
+            }
+        }
+
+        public void updateStats(string text = "")
+        {
+            // 404 error label
+            if (label_404error.InvokeRequired)
+            {
+                label_404error.Invoke(new Action<string>(updateStats), new object[] { text });
+                return;
+            }
+            label_404error.Text = NotFoundError.ToString();
+
+            // sites scanend label
+            if (label_scanned.InvokeRequired)
+            {
+                label_scanned.Invoke(new Action<string>(updateStats), new object[] { text });
+                return;
+            }
+            label_scanned.Text = SitesScanned.ToString();            
+        }
+
+        private void searchIndex(string url)
+        {
+            try
+            {
+                string pageContent = null;
+                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse myres = (HttpWebResponse)myReq.GetResponse();
+
+                using (StreamReader sr = new StreamReader(myres.GetResponseStream()))
+                {
+                    pageContent = sr.ReadToEnd();
+                }
+
+                var match = Regex.Match(pageContent, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                if (match.Success)
+                {
+                    write("Found IP: " + match.Captures[0]);
+
+                    addIp(match.Captures[0].ToString());
+                }
+
+                foreach (Match item in Regex.Matches(pageContent, @"(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"))
+                {
+                    Uri myUri = new Uri(item.Value);
+                    string host = myUri.Host;
+
+                    
+
+                    foreach(string s in blacklistedDomains)
+                    {
+                        if (host.Contains(s))
+                        {
+
+                        }
+                        else
+                        {
+                            write(" ");
+                            write("=========[ " + host + " ]=========");
+
+                            write("url: " + item.Value);
+                            scanSites(item.Value);
+                            updateStats();
+                        }
+                    }
+                }
+            }
+            catch(WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError && ex.Response != null)
+                {
+                    var resp = (HttpWebResponse)ex.Response;
+                    if (resp.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        write("404 Not Found");
+                        NotFoundError++;
+                    }
+                    else
+                    {
+                        // Do something else
+                    }
+                }
+                else
+                {
+                    
+                }
+
+                write("====================================");
+            }
+            updateStats();
+
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            write("Done");
+            write(" ");
+
+            setStatus("Done");
+            searching = false;
+            //button1.Enabled = true;
+        }
+
+        private void scanSites(string url)
+        {
+            try
+            {
+                SitesScanned++;
+
+                Uri myUri = new Uri(url);
+                string host = myUri.Host;
+
+                write(" ");
+                write("=========[ " + host + " ]=========");
+
+
+                string pageContent = null;
+                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse myres = (HttpWebResponse)myReq.GetResponse();
+
+                using (StreamReader sr = new StreamReader(myres.GetResponseStream()))
+                {
+                    pageContent = sr.ReadToEnd();
+                }
+
+                var match = Regex.Match(pageContent, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                if (match.Success)
+                {
+                    write("Found IP: " + match.Captures[0]);
+                    addIp(match.Captures[0].ToString());
+                }
+
+                foreach (Match item in Regex.Matches(pageContent, @"(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"))
+                {
+
+                    foreach (string s in blacklistedDomains)
+                    {
+                        if (host.Contains(s))
+                        {
+
+                        }
+                        else
+                        {
+                            write("url: " + item.Value);
+
+
+                            Task.Run(() => scanSubSites(item.Value));
+                            updateStats();
+                        }
+                    }
+
+                    
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError &&
+        ex.Response != null)
+                {
+                    var resp = (HttpWebResponse)ex.Response;
+                    if (resp.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        write("404 Not Found");
+                        NotFoundError++;
+                    }
+                    else
+                    {
+                        // Do something else
+                    }
+                }
+                else
+                {
+                    // Do something else
+                }
+
+                write("====================================");
+            }
+
+            updateStats();
+        }
+
+        private void scanSubSites(string url)
+        {
+            try
+            {
+                SitesScanned++;
+
+                Uri myUri = new Uri(url);
+                string host = myUri.Host;
+
+                write(" ");
+                write("=========[ " + host + " ]=========");
+
+
+                string pageContent = null;
+                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse myres = (HttpWebResponse)myReq.GetResponse();
+
+                using (StreamReader sr = new StreamReader(myres.GetResponseStream()))
+                {
+                    pageContent = sr.ReadToEnd();
+                }
+
+                var match = Regex.Match(pageContent, @"\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b");
+                if (match.Success)
+                {
+                    write("Found IP: " + match.Captures[0]);
+                    addIp(match.Captures[0].ToString());
+                }
+
+                updateStats();
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError &&
+        ex.Response != null)
+                {
+                    var resp = (HttpWebResponse)ex.Response;
+                    if (resp.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        write("404 Not Found");
+                        NotFoundError++;
+                    }
+                    else
+                    {
+                        // Do something else
+                    }
+                }
+                else
+                {
+                    // Do something else
+                }
+
+                write("====================================");
+            }
+
+            updateStats();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label_status.Text = "Running";
+            searching = true;
+            //button1.Enabled = false;
+
+            textbox_log.Clear();
+            write("Search term: " + textbox_search.Text);
+            write(" ");
+            write(" ");
+            write(" ");
+            write(" ");
+            Task.Run(() => searchIndex("https://www.google.com/search?q=" + textbox_search.Text));
+        }
+
+        private bool checkSSH(string ip)
+        {
+            TcpClient tcpClient = new TcpClient();
+
+            try
+            {
+                tcpClient.Connect(ip, 22);
+                write(ip + " is accepting SSH");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                write(ex.Message);
+                return false;
+            }
+        }
+
+        private void textbox_search_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            }
+        }
+    }
+}
